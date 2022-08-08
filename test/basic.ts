f498ceacd6e2a5387662b5f842016f9acdbced15
leon123858 <a0970785699@gmail.com>
2022-08-09 00:56:23 +0800

    test setting

---
+++ test/basic.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('check program can execute', function () {
+	it('should be right for correct setting', async () => {
+		expect(true).is.true;
+	});
+});
